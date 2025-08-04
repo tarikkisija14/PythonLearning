a53222b81d68e141bb57a99c1a3504c791fb04ff
tarikkisija14 <tarik.kisija@hotmail.com>
2025-08-04 21:24:29 +0200

    Iris

---
+++ Sklearn/Iris_KNeighborsClassifier_train_model.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def LoadIris():
+    iris=load_iris()
+    X=iris.data
+    y=iris.target
+    return X,y
+
+def SplitIris(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def model_training(X_train,y_train,k):
+    model=KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train,y_train)
+    return model
+
+def EvaluateModel(model,X_test,y_test):
+    y_pred=model.predict(X_test)
+    acc=accuracy_score(y_test,y_pred)
+    print(f"Tacnost Modela:{acc:.2f}")
+
+X,y=LoadIris()
+X_train, X_test, y_train, y_test = SplitIris(X,y)
+model = model_training(X_train,y_train,k=3)
+EvaluateModel(model,X_test,y_test)
